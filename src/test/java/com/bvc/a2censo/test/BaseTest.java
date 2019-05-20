25e03f7a6fb17ae558ef99fad12e838ad3342a47
juanpiedrahita <juangonzalez1597@gmail.com>
2019-05-20 19:27:08 +0000

    HU1-CP001: validateing menu and body content with exccel data sheet

--- src/test/java/com/bvc/a2censo/test/BaseTest.java
+++ src/test/java/com/bvc/a2censo/test/BaseTest.java
@@ -1,10 +1,16 @@
 package com.bvc.a2censo.test;
 
 import com.bvc.a2censo.util.BroswerFactory;
+import com.bvc.a2censo.util.ExcelUtils;
+import com.bvc.a2censo.util.Screenshot;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -49,6 +55,55 @@ public abstract  class BaseTest {
 
     public void navegateToLanding(){
         driver.navigate().to(basePath);
+    }
+
+    public WebElement getElementWithExcel(String selector, String selecttValue){
+        WebElement element = null;
+        if (selector.equals("xpath")) {
+            element = driver.findElement(By.xpath(selecttValue));
+        } else if (selector.equals("id")) {
+            element = driver.findElement(By.id(selecttValue));
+        } else if (selector.equals("link")) {
+            element = driver.findElement(By.linkText(selecttValue));
+        } else if (selector.equals("class")) {
+            element = driver.findElement(By.className(selecttValue));
+        } else if (selector.equals("name")) {
+            element = driver.findElement(By.name(selecttValue));
+        }
+        return element;
+    }
+
+    public WebElement getValidateType(String validationType, WebElement element){
+        WebElement elementToValidate = null;
+        if(validationType.equals("visibility")){
+            elementToValidate = wait.until(ExpectedConditions.visibilityOf(element));
+        }
+        return  elementToValidate;
+    }
+
+    public void checkPageContent(String document,String sheet, String testPath, String dataPath){
+        String elementName = "page_content";
+        try {
+            String[][] data = ExcelUtils.getData(dataPath+document+".xlsx",sheet,true);
+            for (int i = 0; i<data.length; i++) {
+                String[] rowData = data[i];
+                elementName = rowData[0];
+                Reporter.log(elementName+" is visible? ");
+                //WebElement element = driver.findElement(By.xpath();
+                WebElement element = getElementWithExcel(rowData[1],rowData[2]);
+                action.moveToElement(element).build().perform();
+                element = getValidateType(rowData[3],element);
+                Reporter.log("Yes <br>");
+                String ssText = Screenshot.takeScreenshot(driver,testPath,elementName+"_visible");
+                Reporter.log(ssText);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Reporter.log(elementName+" is not visible<br>");
+            String ssText = Screenshot.takeScreenshot(driver,testPath,elementName+"_not_visible");
+            Reporter.log(ssText);
+            Assert.fail(elementName+" is not visible");
+        }
     }
 
 }
--- src/test/java/com/bvc/a2censo/test/hu1_001/CP_001.java
+++ src/test/java/com/bvc/a2censo/test/hu1_001/CP_001.java
@@ -1,9 +1,12 @@
 package com.bvc.a2censo.test.hu1_001;
 
 import com.bvc.a2censo.test.BaseTest;
+import com.bvc.a2censo.util.ExcelUtils;
 import com.bvc.a2censo.util.Screenshot;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Coordinates;
+import org.openqa.selenium.interactions.internal.Locatable;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -16,14 +19,13 @@ import javax.swing.*;
 
 public class CP_001 extends BaseTest {
 
-
-
     @Test(description = "This TC will access to a2censo portal")
     @Parameters({"browser","hu"})
     public void CP_001(String broswer, String hu) {
 
         String testCase = hu+"-"+this.getClass().getSimpleName();
         String testPath = hu+"/"+broswer+"/"+this.getClass().getSimpleName();
+        String dataPath = dataBasePath+"/"+hu+"/";
         Reporter.log("Starting test: "+testCase+",with OS: "+operativeSystem+" in "+broswer+"<br>");
 
         Reporter.log("Navigating to "+basePath+"<br>");
@@ -35,20 +37,29 @@ public class CP_001 extends BaseTest {
         Reporter.log("Url shoud be '"+basePath+"'<br>");
         Assert.assertTrue(driver.getCurrentUrl().equals(basePath));
 
-        Reporter.log("Scrolling to the end<br>");
-        jsExecutor.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+        Reporter.log("Validating menu content<br>");
+        checkPageContent("object","menu",testPath,dataPath);
+        Reporter.log("Validating body content<br>");
+        checkPageContent("object","body",testPath,dataPath);
+        checkScrolling(testPath);
+    }
 
-        Reporter.log("Footer is visible?");
+    public void checkScrolling (String testPath) {
         try {
-            WebElement footer = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("footer")));
-            Reporter.log("true <br>");
-            String ssText = Screenshot.takeScreenshot(driver,testPath,"footer_visible");
-            Reporter.log(ssText);
-        } catch (Exception e) {
-            Reporter.log("false <br>");
-            String ssText = Screenshot.takeScreenshot(driver,testPath,"footer__not_visible");
-            Reporter.log(ssText);
-            Assert.fail("Footer is not visible");
+            Reporter.log("Scrolling to the top<br>");
+            jsExecutor.executeScript("window.scrollTo(0, 0)");
+            Thread.sleep(2000);
+            Reporter.log(Screenshot.takeScreenshot(driver,testPath,"scroll_to_top"));
+            Reporter.log("Scrolling to the end<br>");
+            jsExecutor.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+            Thread.sleep(2000);
+            Reporter.log(Screenshot.takeScreenshot(driver,testPath,"scroll_to_footer"));
+            Reporter.log("Scrolling to the top<br>");
+            jsExecutor.executeScript("window.scrollTo(0, 0)");
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
     }
 
