21cd6a717ecd4700c9e1738057f4bab9bbce21df
juanpiedrahita <juangonzalez1597@gmail.com>
2019-05-27 21:24:26 +0000

    Adding hu1-cp-004

---
+++ src/test/java/com/bvc/a2censo/test/cases/hu1_001/CP_004.java
@@ -0,0 +1,30 @@
+package com.bvc.a2censo.test.cases.hu1_001;
+
+import com.bvc.a2censo.test.model.TestBase;
+import com.bvc.a2censo.test.util.CustomReporter;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class CP_004 extends TestBase {
+
+    @Test(description = "This TC will access to a2censo portal")
+    @Parameters({"browser","hu"})
+    public void CP_004(String broswer, String hu) {
+
+        String testCase = hu+"-"+this.getClass().getSimpleName();
+        String testPath = hu+"/"+broswer+"/"+this.getClass().getSimpleName();
+        String dataPath = dataBasePath+"/"+hu+"/"+this.getClass().getSimpleName().replace("_","-")+"/";
+        CustomReporter.title("Starting test: "+testCase+",with OS: "+operativeSystem+" in "+broswer);
+
+        CustomReporter.log("Navigating to "+baseUrl);
+        this.navegateToLanding();
+
+        CustomReporter.subTitle("Validating footer content");
+        checkPageContent("objects","footer",testPath,dataPath);
+
+        CustomReporter.subTitle("Validating footer navigation");
+        CustomReporter.error("Functionality not available yet");
+    }
+
+}
